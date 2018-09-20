c6ef656eb65858a728b0b69577dfcbec6fadd1f6
Serhat Yuna <serhat.yuna@ceng.deu.edu.tr>
2018-09-20 21:49:48 +0300

    game is fckn' over

--- app/index.js
+++ app/index.js
@@ -16,32 +16,31 @@ new Vue({
   },
   methods: {
     makeUsersMove(move) {
-      let movesDOM = document.querySelector('.moves');
-      if (move === 'rock') {
-        this.usersMove = 0;
-        this.isRockSelected = true;
-        this.isPaperSelected = false;
-        this.isScissorsSelected = false;
-        movesDOM.style.justifyContent = 'flex-start';
-        this.isGameOver = true;
-        this.makeOpponentsMove();
-      } else if (move === 'paper') {
-        this.usersMove = 1;
-        this.isRockSelected = false;
-        this.isPaperSelected = true;
-        this.isScissorsSelected = false;
-        this.isGameOver = true;
-        this.makeOpponentsMove();
-      } else if (move === 'scissors') {
-        this.usersMove = 2;
-        this.isRockSelected = false;
-        this.isPaperSelected = false;
-        this.isScissorsSelected = true;
-        movesDOM.style.justifyContent = 'flex-end';
-        this.isGameOver = true;
-        this.makeOpponentsMove();
-      } else {
-        this.play = false;
+      if (this.usersMove === null) {
+        if (move === 'rock') {
+          this.usersMove = 0;
+          this.isRockSelected = true;
+          this.isPaperSelected = false;
+          this.isScissorsSelected = false;
+          this.isGameOver = true;
+          this.makeOpponentsMove();
+        } else if (move === 'paper') {
+          this.usersMove = 1;
+          this.isRockSelected = false;
+          this.isPaperSelected = true;
+          this.isScissorsSelected = false;
+          this.isGameOver = true;
+          this.makeOpponentsMove();
+        } else if (move === 'scissors') {
+          this.usersMove = 2;
+          this.isRockSelected = false;
+          this.isPaperSelected = false;
+          this.isScissorsSelected = true;
+          this.isGameOver = true;
+          this.makeOpponentsMove();
+        } else {
+          this.play = false;
+        }
       }
     },
     makeOpponentsMove() {
@@ -49,14 +48,69 @@ new Vue({
         this.opponentsMove = Math.floor(Math.random() * 3);
         if (this.opponentsMove === 0) {
           this.moveWillBeShown = 'rock';
+          this.determineTheWinner();
         } else if (this.opponentsMove === 1) {
           this.moveWillBeShown = 'paper';
+          this.determineTheWinner();
         } else if (this.opponentsMove === 2) {
           this.moveWillBeShown = 'scissors';
+          this.determineTheWinner();
         } else {
           this.play = false;
         }
       }
-    }
+    },
+    determineTheWinner() {
+      if (this.opponentsMove !== null && this.usersMove !== null) {
+        // Rock - Paper
+        if (this.usersMove === 0 && this.opponentsMove === 1) {
+          this.winner = 'You Lost!';
+          return;
+        }
+
+        // Rock - Scissors
+        if (this.usersMove === 0 && this.opponentsMove === 2) {
+          this.winner = 'You Won!';
+          return;
+        }
+
+        // Paper - Rock
+        if (this.usersMove === 1 && this.opponentsMove === 0) {
+          this.winner = 'You Won!';
+          return;
+        }
+
+        // Paper - Scissors
+        if (this.usersMove === 1 && this.opponentsMove === 2) {
+          this.winner = 'You Lost!';
+          return;
+        }
+
+        // Scissors - Rock
+        if (this.usersMove === 2 && this.opponentsMove === 0) {
+          this.winner = 'You Lost!';
+          return;
+        }
+
+        // Scissors - Paper
+        if (this.usersMove === 2 && this.opponentsMove === 1) {
+          this.winner = 'You Won!';
+          return;
+        }
+
+        this.winner = 'Draw!';
+      }
+    },
+    playAgain() {
+      this.isGameOver = false;
+      this.usersMove = null;
+      this.opponentsMove = null;
+      this.moveWillBeShown = '';
+      this.winner = '';
+      this.isRockSelected = null;
+      this.isPaperSelected = null;
+      this.isScissorsSelected = null;
+      this.play = true;
+    },
   },
 })
